72a0583f19f2654548fee2b339291d06e4698e49
neulhan <sting7675@naver.com>
2019-09-20 22:54:02 +0900

    디지털/아날로그 타이머 js로 구현

--- index.js
+++ index.js
@@ -1,16 +1,75 @@
-window.onload = () => {
-	const app = document.querySelector("#app");
-	const testDiv = document.createElement("div");
-	const practiceOne = document.createElement("p");
-	const practiceTwo = document.createElement("p");
+const timer = document.querySelector('.timer');
+const digital = document.querySelector('.dgt-clock');
+const analog = document.querySelector('.anlg-clock');
 
-	practiceOne.innerText = `지정한 숫자만큼 아날로그 초침이 시계 방향으로 설정된 다음 
-        시작 버튼을 누르면 1초마다 시계 반대 방향으로 움직여서 
-        결국 12시 방향을 가르키며 종료되는 아날로그 타이머`;
-	practiceTwo.innerText = `지정한 숫자만큼 타이머가 표시되고 
-        시작 버튼을 누르면 1초씩 줄어들고 
-        0초에서 정지하는 디지털 타이머`;
+//전역변수로 설정된 timerCount 를 각 함수들에서 1씩 더하고 빼주면서 타이머가 실행됩니다
+let timerCount = 0;
 
-	testDiv.append(practiceOne, practiceTwo);
-	app.prepend(testDiv);
-};
+//HTML 최대한 안쓰고 연습해보려고 init 함수 실행시 버튼들을 생성하는 함수입니다
+function createUi(){
+	const buttonCheck = document.querySelector('.timer');
+	if (buttonCheck.innerHTML === '') {
+		console.log(1);
+		const buttonPlus = document.createElement('button');
+		const buttonMinus = document.createElement('button');
+		const buttonStart = document.createElement('button');
+		const timer = document.querySelector('.timer');
+		buttonPlus.innerText = '+';
+		buttonMinus.innerText = "-";
+		buttonStart.innerText = "start";
+		buttonPlus.id = 'btnPlus';
+		buttonMinus.id = 'btnMinus';
+		buttonStart.id = 'btnStart';
+		timer.appendChild(buttonPlus);
+		timer.appendChild(buttonMinus);
+		timer.appendChild(buttonStart);
+	}
+	digital.innerText = `${timerCount}`;
+}
+// 처리된 결과를 아날로그 시계와 디지털 시계에 시각적으로 반영해주는 함수
+function printTimer(){
+	digital.innerText = `${timerCount}`;
+
+	analog.style.transform = `rotate(${timerCount*6}deg)`;
+}
+
+//타이머 시간이 줄어드는 것을 구현하는 함수
+function countFunc() {
+	timerCount -= 1;
+
+	printTimer();
+	if (timerCount === 0) {
+		timer.classList.remove('displayNone');
+		clearInterval(interval_break);
+	}
+}
+// + - start 버튼을 눌렀을 때 이벤트 핸들러
+function clickHandler(event) {
+	const btn = event.target;
+	console.log(btn.id);
+	//플러스 버튼을 눌렀을 경우
+	if (btn.id === 'btnPlus') {
+		if (timerCount < 60) {
+			timerCount += 1;
+			printTimer();
+		}
+    // 마이너스 버튼을 눌렀을 경우
+	} else if (btn.id === 'btnMinus') {
+		if (timerCount>0){
+			timerCount -= 1;
+			printTimer();
+		}
+	//start 버튼을 눌렀을 경우
+	} else {
+        //1초마다 타이머 카운트해주는 함수 실행
+		window.interval_break = setInterval(countFunc, 1000);
+		timer.classList.add('displayNone');
+	}
+}
+// init
+function init(){
+	createUi();
+	timer.addEventListener('click', clickHandler)
+}
+
+init();
